c3230867e8fb916bf8368482363f9d025ed0ff0d
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2023-04-17 22:36:37 +0300

    organization

---
+++ BFT-Distributed-G-Set-Remote/server/modules/malicious.go
@@ -0,0 +1,35 @@
+package modules
+
+import (
+	"BFT-Distributed-G-Set-Remote/config"
+	"BFT-Distributed-G-Set-Remote/messaging"
+	"BFT-Distributed-G-Set-Remote/server"
+	"BFT-Distributed-G-Set-Remote/tools"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func StartMalicious(servers []config.Node, default_port, num_threads int) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	hostname = strings.Split(hostname, ".")[0]
+	for i := default_port; i < default_port+num_threads; i++ {
+		go func(my_port int) {
+			p := strconv.Itoa(my_port)
+			me := config.Node{Host: hostname, Port: p}
+			s := server.CreateServer(me, servers)
+			tools.Log(s.Id, "Started with MALICIOUS behaviour")
+			for {
+				msg, err := s.Receive_socket.RecvMessage(0)
+				if err != nil {
+					tools.Log(s.Id, err.Error())
+					return
+				}
+				messaging.HandleMessageByzantine(s, msg, "MALICIOUS")
+			}
+		}(i)
+	}
+}
