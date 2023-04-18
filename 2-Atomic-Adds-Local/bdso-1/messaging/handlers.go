c0327b3820f021b51eb48c3dc360ce9154c54b13
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2023-04-18 08:40:32 +0300

    bug fixing

--- 2-Atomic-Adds-Local/bdso-1/messaging/handlers.go
+++ 2-Atomic-Adds-Local/bdso-1/messaging/handlers.go
@@ -56,18 +56,25 @@ func handleRB(receiver *server.Server, message Message) {
 		return
 	}
 
+	// delivered := HandleReliableBroadcast(receiver, message)
+	// if delivered && !gset.Exists(receiver.Gset, message.Content[1]) {
+	// 	gset.Add(receiver.Gset, message.Content[1])
+	// 	receiver.Receive_socket.SendMessage(response)
+	// 	tools.Log(receiver.Id, "Appended record {"+message.Content[1]+"}")
+	// 	return
+	// }
+
 	delivered := HandleReliableBroadcast(receiver, message)
-	if delivered && !gset.Exists(receiver.Gset, message.Content[1]) {
-		// now check if atomic
+	if delivered {
 		gset.Add(receiver.Gset, message.Content[1])
 		receiver.Receive_socket.SendMessage(response)
 		tools.Log(receiver.Id, "Appended record {"+message.Content[1]+"}")
 		return
 	}
 
-	if delivered && gset.Exists(receiver.Gset, message.Content[1]) {
-		receiver.Receive_socket.SendMessage(response)
-		tools.Log(receiver.Id, "Record {"+message.Content[1]+"} already exists")
-		return
-	}
+	// if delivered && gset.Exists(receiver.Gset, message.Content[1]) {
+	// 	receiver.Receive_socket.SendMessage(response)
+	// 	tools.Log(receiver.Id, "Record {"+message.Content[1]+"} already exists")
+	// 	return
+	// }
 }
